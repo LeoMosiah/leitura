f0b65897c7756bd24d0738d3f5fbd2e98aec6b6e
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-06 15:50:58 -0200

    Update PostContainer.js

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 import Post from "./Post";
+import { connect } from "react-redux";
 
 class PostContainer extends Component {
   render() {
+    const { posts, authedUser } = this.props;
     return (
       <div>
         <ul>
-          <li>
-              <Post/>
-          </li>
+          {Object.values(posts).map(post => (
+            <li key={post.id}>
+              <Post post={post} />
+            </li>
+          ))}
         </ul>
       </div>
     );
   }
 }
+function mapStateToProps({ posts, authedUser }) {
+  return {
+    authedUser,
+    posts
+  };
+}
 
-export  default PostContainer;
+export default connect(mapStateToProps)(PostContainer);
