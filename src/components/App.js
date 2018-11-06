d91b66d1058ea284c0e4c00cda03f01c9cc6ab35
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-06 15:51:55 -0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,26 @@ import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Home from "./Home";
 import * as API from "../utils/api";
-import { Pane} from "evergreen-ui";
+import { Pane } from "evergreen-ui";
 
 class App extends Component {
   state = {
-    categories: []
+    categories: [],
+    data: []
   };
   componentDidMount() {
     this.props.dispatch(handleInitialData());
-    this.setState({
-      categories: API.getCategories()
-    });
+    this.setState({ categories: API.getCategories() });
   }
+
   render() {
     return (
-      <Pane paddingTop={15} paddingBottom={15} paddingLeft={50} paddingRight={50}>
+      <Pane
+        paddingTop={15}
+        paddingBottom={15}
+        paddingLeft={50}
+        paddingRight={50}
+      >
         <Home />
       </Pane>
     );
