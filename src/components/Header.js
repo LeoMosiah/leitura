0f614ad97ef458c5d37d35d2b2aa872f98413a51
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-07 14:58:17 -0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,76 @@
 import React, { Component } from "react";
-import { Pane, Button, SearchInput, Paragraph, Link } from "evergreen-ui";
+import {
+  Pane,
+  Button,
+  SearchInput,
+  Paragraph,
+  Link,
+  Dialog
+} from "evergreen-ui";
+import SignInForm from "./SingInForm";
 
 class Header extends Component {
+  state = {
+    dialogIsShown: false
+  };
+  handleClick() {}
   render() {
+    const { categories } = this.props;
     return (
-      <Pane>  
-      <Pane  display="flex">
-        <Pane flex={1}>
-        <Paragraph marginTop={5} size={500} color="dark">
-            Leitura
-        </Paragraph>
+      <Pane>
+        <Pane display="flex">
+          <Pane flex={1}>
+            <Paragraph marginTop={5} size={500} color="dark">
+              Leitura
+            </Paragraph>
+          </Pane>
+          <Pane>
+            <SearchInput placeholder="Search Post" marginRight={6} />
+            <Button
+              marginRight={6}
+              marginBottom={4}
+              appearance="primary"
+              intent="success"
+              onClick={() => this.setState({ dialogIsShown: true })}
+            >
+              Log In
+            </Button>
+            <Button
+              marginRight={6}
+              marginBottom={4}
+              appearance="primary"
+              intent="warning"
+            >
+              Sign up
+            </Button>
+          </Pane>
         </Pane>
-        <Pane>
-          <SearchInput placeholder="Search Post" />
-          <Button
-            marginRight={6}
-            marginBottom={4}
-            appearance="minimal"
-            intent="success"
-          >
-            Sign in
-          </Button>
-          <Button
-            marginRight={6}
-            marginBottom={4}
-            appearance="minimal"
-            intent="success"
-          >
-            Sign up
-          </Button>
+        <Pane
+          display="inline-flex"
+          width="100%"
+          className="navigation-bar__categories"
+        >
+          {categories.map(category => (
+            <Button
+              marginRight={12}
+              appearance="minimal"
+              intent="success"
+              className="navigation-bar__category"
+              key={category.name}
+            >
+              {category.name.toUpperCase()}
+            </Button>
+          ))}
         </Pane>
-      </Pane>
-        <Pane display="inline-flex" width="100%" className="navigation-bar__categories">
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">Redux</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">React</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">React-Native</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">React-Redux</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">Components</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">Category</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">Category</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">Category</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">Category</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">Category</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">Category</Link>
-            <Link href="#" marginRight={12} color="neutral" className="navigation-bar__category">Category</Link>
-        </Pane>    
+        <Dialog
+          isShown={this.state.dialogIsShown}
+          title="Sign in to your Leitura's account!"
+          onCloseComplete={() => this.setState({ dialogIsShown: false })}
+          confirmLabel="Custom Label"
+          hasFooter={false}
+        >
+          <SignInForm />
+        </Dialog>
       </Pane>
     );
   }
