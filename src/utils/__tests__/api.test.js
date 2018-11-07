f248744067b20e7ae6daf8ea423882b13b13e5a8
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-07 14:56:50 -0200

    Update api.test.js

--- src/utils/__tests__/api.test.js
+++ src/utils/__tests__/api.test.js
@@ -1,19 +1,38 @@
-import { tranformArrayOfCommentsIntoMap } from "../api";
+import { tranformArrayIntoMap } from "../helper";
 
-describe("api", () => {
+describe("helper", () => {
+  it("maps array of posts into dictionary", () => {
+    const expectedDictionary = {
+      1: {
+        body: "body_1",
+        id: 1
+      },
+      2: {
+        body: "body_2",
+        id: 2
+      }
+    };
+    const array = [{ id: 1, body: "body_1" }, { id: 2, body: "body_2" }];
+
+    const arrayAsDictionary = tranformArrayIntoMap(array);
+
+    expect(arrayAsDictionary).toEqual(expectedDictionary);
+  });
   it("maps array of comments into dictionary", () => {
     const expectedDictionary = {
       1: {
-        body: "body_1"
+        body: "body_1",
+        id: 1
       },
       2: {
-        body: "body_2"
+        body: "body_2",
+        id: 2
       }
     };
-    const comments = [{ id: 1, body: "body_1" }, { id: 2, body: "body_2" }];
+    const array = [[{ id: 1, body: "body_1" }, { id: 2, body: "body_2" }]];
 
-    const commentsAsDictionary = tranformArrayOfCommentsIntoMap(comments);
+    const arrayAsDictionary = tranformArrayIntoMap(array);
 
-    expect(commentsAsDictionary).toEqual(expectedDictionary);
+    expect(arrayAsDictionary).toEqual(expectedDictionary);
   });
 });
