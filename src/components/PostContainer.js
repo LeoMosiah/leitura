e151c7bf02dacb82016287c58191e95b0d03f2de
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-07 14:59:53 -0200

    Update PostContainer.js

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -2,17 +2,22 @@ import React, { Component } from "react";
 import Post from "./Post";
 import { connect } from "react-redux";
 
+const byCategory = post => post.category === "redux";
+
 class PostContainer extends Component {
   render() {
     const { posts, authedUser } = this.props;
+    const category = "redux";
     return (
       <div>
         <ul>
-          {Object.values(posts).map(post => (
-            <li key={post.id}>
-              <Post post={post} />
-            </li>
-          ))}
+          {Object.values(posts)
+            .filter(byCategory)
+            .map(post => (
+              <li key={post.id}>
+                <Post post={post} />
+              </li>
+            ))}
         </ul>
       </div>
     );
