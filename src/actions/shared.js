74bc7d772befdf3d7ad181ef188ef61c832b43c4
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-07 15:49:19 -0200

    Update shared.js

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,7 @@
 import { receiveUser } from "./users";
 import { receivePosts } from "./posts";
 import { receiveComments } from "./comments";
+import { receiveCategories } from "./categories";
 import { setAuthedUser } from "./authedUser";
 import { getInitialData } from "../utils/api";
 
@@ -8,10 +9,11 @@ const AUTHED_ID = "leomosiah";
 
 export function handleInitialData() {
   return dispatch => {
-    return getInitialData().then(({ posts, comments }) => {
+    return getInitialData().then(({ posts, comments, categories }) => {
       dispatch(receivePosts(posts));
       dispatch(setAuthedUser(AUTHED_ID));
       dispatch(receiveComments(comments));
+      dispatch(receiveCategories(categories));
     });
   };
 }
