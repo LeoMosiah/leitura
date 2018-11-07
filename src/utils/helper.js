0126d9a35d2ec0363737a78ad51016b92d9f118a
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-07 15:50:18 -0200

    Update helper.js

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -2,5 +2,10 @@ import _ from "lodash";
 
 export const tranformArrayIntoMap = array => {
   const flattenArray = _.flatten(array);
-  return _.assign({}, ...flattenArray.map(elem => ({ [elem.id]: elem })));
+  return _.assign(
+    {},
+    ...flattenArray.map(
+      elem => (elem.id ? { [elem.id]: elem } : { [elem.name]: elem })
+    )
+  );
 };
