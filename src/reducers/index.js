3d4ffcbb6f8290f8962180e9a4632892de00e0f1
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-07 15:51:45 -0200

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import authedUser from "./authedUser";
 import comments from "./comments";
 import posts from "./posts";
+import categories from "./categories";
 
 export default combineReducers({
   authedUser,
   posts,
-  comments
+  comments,
+  categories
 });
