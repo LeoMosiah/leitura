83bac231a12332f3983f3b23a94284e7b3742005
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-19 15:48:17 -0200

    Update posts.test.js

--- src/__tests__/reducers/posts.test.js
+++ src/__tests__/reducers/posts.test.js
@@ -1,26 +1,108 @@
-import posts from "../../reducers/posts";
+import postsReducer from "../../reducers/posts";
+import { receivePosts, addPost, removePost } from "../../actions/posts";
 
 describe("Post reducer", function() {
   it("should get initial state", function() {
-    const initialState = { comments: {}, authedUser: null };
+    const initialState = {};
 
-    const expectedState = {
-      comments: {},
-      authedUser: null,
-      posts: { post_id: { id: "id", body: "body", title: "title" } }
+    const postsFetch = {
+      "id 1": {
+        id: "id 1",
+        body: "body 1",
+        title: "title 1"
+      },
+      "id 2": {
+        id: "id 2",
+        body: "body 2",
+        title: "title 2"
+      }
     };
-
-    const action = {
-      type: "RECEIVE_POSTS",
-      posts: {
-        post_id: {
-          id: "id",
-          body: "body",
-          title: "title"
-        }
+    const expectedState = {
+      "id 1": {
+        id: "id 1",
+        body: "body 1",
+        title: "title 1"
+      },
+      "id 2": {
+        id: "id 2",
+        body: "body 2",
+        title: "title 2"
+      }
+    };
+    expect(postsReducer(initialState, receivePosts(postsFetch))).toEqual(
+      expectedState
+    );
+  });
+  it("should add a post to the state", function() {
+    const initialState = {
+      "id 1": {
+        id: "id 1",
+        body: "body 1",
+        title: "title 1"
+      },
+      "id 2": {
+        id: "id 2",
+        body: "body 2",
+        title: "title 2"
       }
     };
 
-    expect(posts(initialState, action)).toEqual(expectedState);
+    const post = { id: "id 3", body: "body 3", title: "title 3" };
+
+    const expecteState = {
+      "id 1": {
+        id: "id 1",
+        body: "body 1",
+        title: "title 1"
+      },
+      "id 2": {
+        id: "id 2",
+        body: "body 2",
+        title: "title 2"
+      },
+      "id 3": {
+        id: "id 3",
+        body: "body 3",
+        title: "title 3"
+      }
+    };
+
+    expect(postsReducer(initialState, addPost(post))).toEqual(expecteState);
+  });
+
+  it("should remove a post to the state", function() {
+    const initialState = {
+      "id 1": {
+        id: "id 1",
+        body: "body 1",
+        title: "title 1"
+      },
+      "id 2": {
+        id: "id 2",
+        body: "body 2",
+        title: "title 2"
+      },
+      "id 3": {
+        id: "id 3",
+        body: "body 3",
+        title: "title 3"
+      }
+    };
+
+    const expecteState = {
+      "id 1": {
+        id: "id 1",
+        body: "body 1",
+        title: "title 1"
+      },
+      "id 2": {
+        id: "id 2",
+        body: "body 2",
+        title: "title 2"
+      }
+    };
+
+    const id = "id 3";
+    expect(postsReducer(initialState, removePost(id))).toEqual(expecteState);
   });
 });
