4c3e25dfe5ee27c83cfd146d1d2b5bfafb5efd5b
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-19 13:09:31 -0200

    Update comments.js

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,8 +1,33 @@
+import { generateUID, saveComment } from "../utils/api";
+
 export const RECEIVE_COMMENTS = "RECEIVE_COMMENTS";
+export const ADD_COMMENT = "ADD_COMMENT";
 
 export function receiveComments(comments) {
   return {
     type: RECEIVE_COMMENTS,
     comments
+  };
+}
+
+function addComment(comment) {
+  return {
+    type: ADD_COMMENT,
+    comment
+  };
+}
+
+export function handleAddComment(comment) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+    const newComment = {
+      ...comment,
+      author: authedUser,
+      id: generateUID(),
+      timestamp: Date.now()
+    };
+    return saveComment(newComment).then(comment =>
+      dispatch(addComment(comment))
+    );
   };
 }
