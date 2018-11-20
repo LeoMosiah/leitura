20f8f5b7a621a0626a78c20be379d1c037e40bcf
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-20 15:28:38 -0200

    Update comments.js

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,7 +1,9 @@
-import { generateUID, saveComment } from "../utils/api";
+import { generateUID, saveComment, deleteComment } from "../utils/api";
 
 export const RECEIVE_COMMENTS = "RECEIVE_COMMENTS";
 export const ADD_COMMENT = "ADD_COMMENT";
+export const REMOVE_COMMENT = "REMOVE_POST";
+export const TOGGLE_COMMENT = "TOGGLE_POST";
 
 export function receiveComments(comments) {
   return {
@@ -10,10 +12,25 @@ export function receiveComments(comments) {
   };
 }
 
-function addComment(comment) {
+export function addComment(comment) {
   return {
     type: ADD_COMMENT,
     comment
+  };
+}
+
+export function toggleComment(comment, option) {
+  return {
+    type: TOGGLE_COMMENT,
+    comment,
+    option
+  };
+}
+
+export function removeComment(id) {
+  return {
+    type: REMOVE_COMMENT,
+    id
   };
 }
 
@@ -29,5 +46,11 @@ export function handleAddComment(comment) {
     return saveComment(newComment).then(comment =>
       dispatch(addComment(comment))
     );
+  };
+}
+
+export function handleDeleteComment(id) {
+  return dispatch => {
+    return deleteComment(id).then(id => dispatch(removeComment(id)));
   };
 }
