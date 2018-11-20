521ba5d365682977ab128077254ef7b712db65c2
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-11-20 15:28:00 -0200

    Update comments.js

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,17 +1,49 @@
-import { RECEIVE_COMMENTS, ADD_COMMENT } from "../actions/comments";
+import {
+  RECEIVE_COMMENTS,
+  ADD_COMMENT,
+  TOGGLE_COMMENT,
+  REMOVE_COMMENT
+} from "../actions/comments";
+
+import _ from "lodash";
+
+const removeComment = (state, action) => {
+  const clonedState = _.cloneDeep(state);
+  delete clonedState[action.id];
+  return clonedState;
+};
+
+const addComment = (state, action) => {
+  let clonedState = _.cloneDeep(state);
+  clonedState[action.comment.id] = action.comment;
+  return clonedState;
+};
+
+const receiveComments = (state, action) => {
+  let clonedState = _.cloneDeep(state);
+  clonedState = action.comments;
+  return clonedState;
+};
+
+const toggleComment = (state, action) => {
+  let clonedState = _.cloneDeep(state);
+  clonedState[action.comment.id].voteScore =
+    action.option === "upVote"
+      ? action.comment.voteScore + 1
+      : action.comment.voteScore - 1;
+  return clonedState;
+};
 
 export default function comments(state = {}, action) {
   switch (action.type) {
     case RECEIVE_COMMENTS:
-      return {
-        ...state,
-        ...action.comments
-      };
+      return receiveComments(state, action);
     case ADD_COMMENT:
-      return {
-        ...state,
-        [action.comment.id]: action.comment
-      };
+      return addComment(state, action);
+    case REMOVE_COMMENT:
+      return removeComment(state, action);
+    case TOGGLE_COMMENT:
+      return toggleComment(state, action);
     default:
       return state;
   }
