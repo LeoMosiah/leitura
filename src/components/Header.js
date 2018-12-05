c33d467ffb8887576c36653f3e84e36e151129f2
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-12-05 11:19:51 -0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import { styles } from "./styles/headerStyles";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import IconButton from "@material-ui/core/IconButton/IconButton";
+import SimpleModalWrapped from "./Modal";
 
 function Authentication(props) {
   const { authedUser, handleSignOut } = props;
@@ -26,53 +27,71 @@ function Authentication(props) {
     );
 }
 
-function Header(props) {
-  const { classes, authedUser, handleOpenModal, handleSignOut } = props;
-  const NewPostButton = authedUser => {
-    if (authedUser) {
-      return (
-        <Link to="/post/new">
-          <Button
-            variant="outlined"
-            color="primary"
-            size="small"
-            onClick={() => handleOpenModal()}
-          >
-            New Post
-          </Button>
-        </Link>
-      );
-    } else {
-      return (
-        <Button
-          variant="outlined"
-          color="primary"
-          size="small"
-          onClick={() => handleOpenModal()}
-        >
+function NewPostButton(props) {
+  const { authedUser, handleOpenModal } = props;
+  if (authedUser) {
+    return (
+      <Link to="/post/new">
+        <Button variant="outlined" color="primary" size="small">
           New Post
         </Button>
-      );
-    }
+      </Link>
+    );
+  } else {
+    return (
+      <Button
+        variant="outlined"
+        color="primary"
+        size="small"
+        onClick={() => handleOpenModal()}
+      >
+        New Post
+      </Button>
+    );
+  }
+}
+
+class Header extends React.Component {
+  state = {
+    modalIsOpen: false
   };
-  return (
-    <header>
-      <Toolbar className={classes.toolbarMain}>
-        {NewPostButton(authedUser)}
-        <Typography
-          component="h2"
-          variant="h5"
-          color="primary"
-          align="center"
-          noWrap
-          className={classes.toolbarTitle}
-        >
-          <Link to="/">Leitura</Link>
-        </Typography>
-        <Authentication authedUser={authedUser} handleSignOut={handleSignOut} />
-      </Toolbar>
-    </header>
-  );
+  handleOpenModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+  handleCloseModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+  render() {
+    const { classes, authedUser, handleSignOut } = this.props;
+    return (
+      <header>
+        <Toolbar className={classes.toolbarMain}>
+          <NewPostButton
+            authedUser={authedUser}
+            handleOpenModal={this.handleOpenModal}
+          />
+          <Typography
+            component="h2"
+            variant="h5"
+            color="primary"
+            align="center"
+            noWrap
+            className={classes.toolbarTitle}
+          >
+            <Link to="/">Leitura</Link>
+          </Typography>
+          <Authentication
+            authedUser={authedUser}
+            handleSignOut={handleSignOut}
+          />
+        </Toolbar>
+        <SimpleModalWrapped
+          modalIsOpen={this.state.modalIsOpen}
+          handleCloseModal={this.handleCloseModal}
+        />
+      </header>
+    );
+  }
 }
 
 export default withStyles(styles)(Header);
