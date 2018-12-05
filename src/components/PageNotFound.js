84b64bc48987748365d1d438509f5696d1b6fc5e
LeoMosiah <37636805+LeoMosiah@users.noreply.github.com>
2018-12-05 11:18:35 -0200

    Update PageNotFound.js

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -5,8 +5,7 @@ import { styles } from "./styles/pageNotFound";
 import * as PropTypes from "prop-types";
 import Paper from "@material-ui/core/es/Paper/Paper";
 
-function PageNotFound(props) {
-  const { classes } = props;
+function PageNotFound({ classes }) {
   return (
     <Paper className={classes.container}>
       <div className={classes.frame}>
